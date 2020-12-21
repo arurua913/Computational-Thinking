4da86455168916959cd0989b9fb195d0fde81543
arurua913 <71250122+arurua913@users.noreply.github.com>
2020-12-21 14:32:36 +0800

    Add files via upload

---
+++ 王心如-week15.py
@@ -0,0 +1,30 @@
+
+import turtle 
+shelly = turtle.Turtle() 
+shelly.shape("turtle")
+shelly.position()
+
+shelly.color("black", "green") 
+
+shelly.pencolor("brown")
+
+shelly.turtlesize(7,9,2) 
+shelly.resizemode("auto")
+shelly.turtlesize(5,5,3) 
+shelly.forward(200)
+
+shelly.left(90)
+
+shelly.penup()
+
+shelly.pendown()
+shelly.hideturtle()
+shelly.begin_fill()
+shelly.fillcolor("pink")
+shelly.circle(250) 
+shelly.circle(250, 180, 30) 
+shelly.end_fill()
+
+shelly.stamp()
+
+shelly.write("周雅杰我愛你")
